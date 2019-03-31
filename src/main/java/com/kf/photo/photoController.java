e0658291f087a83a545bce8864959d3c5891a118
LeiDaGou11 <2628655607@qq.com>
2019-03-31 10:56:11 +0800

    LeiDaGou

--- src/main/java/com/kf/photo/photoController.java
+++ src/main/java/com/kf/photo/photoController.java
@@ -1,7 +1,10 @@
 package com.kf.photo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class photoController {
@@ -9,6 +12,12 @@ public class photoController {
     @RequestMapping("")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("upload")
+    @ResponseBody
+    public String upload(MultipartFile photo) {
+        return "aa";
     }
 
 }
